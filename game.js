cfd7b4d1dc27ec93ce258d77c8e3657f189436b1
rcm264 <rcm264@nau.edu>
2019-10-10 10:05:29 -0700

    Added game.js and dependencies

---
+++ game.js
@@ -0,0 +1,21 @@
+var gameport = document.getElementById("gameport");
+
+// Frame init
+var WIDTH = 1000;
+var HEIGHT = 400;
+var renderer = PIXI.autoDetectRenderer({width: WIDTH, height: HEIGHT, backgroundColor: 0x1a52ff});
+gameport.appendChild(renderer.view);
+var stage = new PIXI.Container();
+
+// Menu stage
+var menuStage = new PIXI.Container();
+stage.addChild(menuStage);
+var menuBackground = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Backgrounds/background-menu.png"));
+menuStage.addChild(menuBackground);
+
+function animate()
+{
+    requestAnimationFrame(animate);
+    renderer.render(stage);
+}
+animate();
