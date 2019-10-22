9cd7f8d4b740030b7ca71867c96a6b1f546f53d0
rcm264 <rcm264@nau.edu>
2019-10-22 10:56:27 -0700

    Updated menu

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var renderer = PIXI.autoDetectRenderer({width: WIDTH, height: HEIGHT, background
 gameport.appendChild(renderer.view);
 var stage = new PIXI.Container();
 
-// Menu stage
+/// Menu Stage ////////////////////////
 var menuStage = new PIXI.Container();
 stage.addChild(menuStage);
 var menuBackground = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Backgrounds/background-menu.png"));
@@ -16,29 +16,11 @@ menuStage.addChild(menuBackground);
 // Add play Button
 var playButton = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Buttons/button-play.png"));
 playButton.anchor.set(0.5);
-playButton.position.set(250,100);
+playButton.position.set(WIDTH/2,HEIGHT/4);
 playButton.interactive = true;
 playButton.buttonMode = true;
 playButton.on('mousedown', playButtonHandler);
 menuStage.addChild(playButton);
-
-// Add credits Button
-var creditsButton = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Buttons/creditsBtn.png"));
-creditsButton.anchor.set(0.5);
-creditsButton.position.set(250,200);
-creditsButton.interactive = true;
-creditsButton.buttonMode = true;
-creditsButton.on('mousedown', creditsButtonHandler);
-menuStage.addChild(creditsButton);
-
-// Add instructions Button
-var instructionsButton = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Buttons/htpBtn.png"));
-instructionsButton.anchor.set(0.5);
-instructionsButton.position.set(250,300);
-instructionsButton.interactive = true;
-instructionsButton.buttonMode = true;
-instructionsButton.on('mousedown', instructionsButtonHandler);
-menuStage.addChild(instructionsButton);
 
 // Handles mouse click on play button
 function playButtonHandler(e)
@@ -47,21 +29,91 @@ function playButtonHandler(e)
   stage.addChild(gameStage);    // Go to game stage
 }
 
+// Add instructions Button
+var instrButton = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Buttons/button-instructions.png"));
+instrButton.anchor.set(0.5);
+instrButton.position.set(WIDTH/2,2*HEIGHT/4);
+instrButton.interactive = true;
+instrButton.buttonMode = true;
+instrButton.on('mousedown', instrButtonHandler);
+menuStage.addChild(instrButton);
+
 // Handles mouse click on instructions button
-//TODO: Wire up to background-instructions
-function instructionsButtonHandler(e)
+function instrButtonHandler(e)
 {
-  stage.removeChild(menuStage); // leave main menu
-  stage.addChild(gameStage);    // Go to game stage
+  stage.removeChild(menuStage); // Leave main menu
+  stage.addChild(instrStage);   // Go to instructions menu
 }
 
+// Add credits Button
+var creditButton = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Buttons/button-credits.png"));
+creditButton.anchor.set(0.5);
+creditButton.position.set(WIDTH/2,3*HEIGHT/4);
+creditButton.interactive = true;
+creditButton.buttonMode = true;
+creditButton.on('mousedown', creditButtonHandler);
+menuStage.addChild(creditButton);
+
 // Handles mouse click on credits button
-//TODO: Wire up to background-credits
-function creditsButtonHandler(e)
+function creditButtonHandler(e)
 {
-  stage.removeChild(menuStage); // leave main menu
-  stage.addChild(gameStage);    // Go to game stage
+  stage.removeChild(menuStage); // Leave main menu
+  stage.addChild(creditStage);  // Go to credits screen
 }
+/// END of Menu stage /////////////////
+
+/// Instructions Stage ////////////////
+var instrStage = new PIXI.Container();
+var instrBackground = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Backgrounds/background-menu.png"));
+instrStage.addChild(instrBackground);
+var returnButton = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Buttons/button-return.png"));
+returnButton.anchor.set(1.0);
+returnButton.position.set(WIDTH, HEIGHT);
+returnButton.interactive = true;
+returnButton.buttonMode = true;
+returnButton.on('mousedown', returnButtonHandler);
+instrStage.addChild(returnButton);
+
+// Handles mouse click on return button
+function returnButtonHandler(e)
+{
+  stage.removeChild(instrStage);  // Leave instructions menu
+  stage.addChild(menuStage);      // Go to main menu
+}
+
+// Instructions text
+var instrText = new PIXI.Text('INSTRUCTIONS\nGO\nHERE');
+instrText.anchor.set(0.5);
+instrText.position.set(WIDTH/2,HEIGHT/2);
+instrStage.addChild(instrText);
+/// END of instrucions Stage //////////
+
+/// Credits Stage ////////////////////
+var creditStage = new PIXI.Container();
+var creditBackground = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Backgrounds/background-menu.png"));
+creditStage.addChild(creditBackground);
+var creditReturnButton = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Buttons/button-return.png"));
+creditReturnButton.anchor.set(1.0);
+creditReturnButton.position.set(WIDTH, HEIGHT);
+creditReturnButton.interactive = true;
+creditReturnButton.buttonMode = true;
+creditReturnButton.on('mousedown', creditReturnButtonHandler);
+creditStage.addChild(creditReturnButton);
+
+// Handles mouse click on return button
+function creditReturnButtonHandler(e)
+{
+  stage.removeChild(creditStage);  // Leave credits screen
+  stage.addChild(menuStage);       // Go to main menu
+}
+
+// Credits text
+var creditText = new PIXI.Text('CREDITS GO HERE');
+creditText.anchor.set(0.5);
+creditText.position.set(WIDTH/2,HEIGHT/2);
+creditStage.addChild(creditText);
+/// END of Credits Stage //////////////
+
 
 /// Game Stage ////////////////////////
 var gameStage = new PIXI.Container();
@@ -70,6 +122,8 @@ gameGround.position.set(0,350);
 var gameSky = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Backgrounds/background-game-sky.png"));
 gameStage.addChild(gameGround);
 gameStage.addChild(gameSky);
+/// End of game stage /////////////////
+
 
 
 // Load runner
